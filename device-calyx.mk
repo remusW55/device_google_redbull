c3e7af75c601ec589544b63e7ab0dc19b1f09986
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-22 15:13:23 -0700

    redbull: Add privapp permissions for Google EUICC apps
    Change-Id: I2e7bc09bac103d31132bd6ae646cb2f6d7ba8b19

--- device-calyx.mk
+++ device-calyx.mk
@@ -16,4 +16,5 @@
 
 # EUICC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+    device/google/redbull/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml

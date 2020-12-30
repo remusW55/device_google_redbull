228cfec57ba9872616a6245c54a54b23f011e932
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:28 +0530

    The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-calyx.mk
+++ device-calyx.mk
@@ -19,3 +19,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/redbull/overlay-calyx
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/redbull/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
+
+# For Google Camera
+PRODUCT_COPY_FILES += \
+    device/google/redbull/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml

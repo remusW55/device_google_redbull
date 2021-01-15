dc0852a8d170c2d581b43e797b08a11c62d8a7c9
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-16 04:28:12 +0530

    Update module list after disabling DEBUG_FS
    * DEBUG_FS is not allowed on non-debuggable builds
    
    Change-Id: I822eec36f0be1d00a103d9de417e4c6ded96e8b5

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -150,7 +150,6 @@ BOOT_KERNEL_MODULES := \
 	subsystem_notif.ko \
 	ramdump.ko \
 	msm_memshare.ko \
-	msm_bus_dbg_rpmh.ko \
 	msm_bus_rpmh.ko \
 	rpmh-regulator.ko \
 	qcom-geni-se.ko \
@@ -312,7 +311,6 @@ BOOT_KERNEL_MODULES := \
 	leds-qpnp-flash-common.ko \
 	leds-qpnp-flash-v2.ko \
 	of_slimbus.ko \
-	tz_log.ko \
 	google_wlan_mac.ko \
 	msm_ext_display.ko \
 	msm_qmp.ko \

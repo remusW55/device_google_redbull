dad98d4d8b61e2dd33499a97176e3505a84ac309
Chirayu Desai <chirayudesai1@gmail.com>
2021-09-08 22:00:48 +0530

    Revert "The full Google Pixel experience!"
    This reverts commit 228cfec57ba9872616a6245c54a54b23f011e932.
    
    * Move to devices
    
    Change-Id: Ia4ebc3499fa7ae63ca37587dafc202d9703fc984

--- device-calyx.mk
+++ device-calyx.mk
@@ -20,10 +20,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/redbull/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
 
-# For Google Camera
-PRODUCT_COPY_FILES += \
-    device/google/redbull/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys-intf/display \

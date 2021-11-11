03121cebe8adc9495275dfc3e411e41260ed275d
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    device-calyx: Set TARGET_PREBUILT_KERNEL
    Change-Id: Ie20029a999243dc0eb675fac2309078a6a19e1ee

--- device-calyx.mk
+++ device-calyx.mk
@@ -4,3 +4,5 @@ DEVICE_PACKAGE_OVERLAYS += device/google/redbull/overlay-calyx
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/redbull/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
+TARGET_PREBUILT_KERNEL := device/google/redbull-kernel/Image.lz4

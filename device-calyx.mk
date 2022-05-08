d92ffb1d68bfc4890a6893c5b7f1b30452b9a63d
Michael Bestas <mkbestas@gmail.com>
2022-05-08 03:45:18 +0300

    Add privapp permissions for HbmSVManager
    Change-Id: Ieab465f478424d8a936519250b40832f1c6bbc33

--- device-calyx.mk
+++ device-calyx.mk
@@ -5,6 +5,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/redbull/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
 
+# HBM
+PRODUCT_COPY_FILES += \
+    device/google/redbull/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
+
 TARGET_PREBUILT_KERNEL := device/google/redbull-kernel/Image.lz4
 
 # Build necessary packages for system_ext
